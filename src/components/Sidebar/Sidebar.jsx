075fec64403998f2829f832cbb91242efa94a9bd
ZainabFara <zainabfarah050@gmail.com>
2024-09-09 10:24:16 +0200

    Added sidebar component with navigation. Used fontAwesome for icons

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faSignInAlt, faTasks } from '@fortawesome/free-solid-svg-icons';  
+import styles from './Sidebar.module.css';  
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <ul className={styles.sidebarList}>
+        <li className={styles.sidebarItem}>
+          <a href="#Home">
+            <FontAwesomeIcon icon={faHome} className={styles.icon} />
+            Home
+          </a>
+        </li>
+        <li className={styles.sidebarItem}>
+          <a href="#Login">
+            <FontAwesomeIcon icon={faSignInAlt} className={styles.icon} />
+            Login
+          </a>
+        </li>
+        <li className={styles.sidebarItem}>
+          <a href="#todo">
+            <FontAwesomeIcon icon={faTasks} className={styles.icon} />
+            To-do
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
