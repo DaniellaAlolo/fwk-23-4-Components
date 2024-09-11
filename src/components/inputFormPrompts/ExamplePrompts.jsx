5229cb0389e60852042263a1edd0d3b5dc344669
Musa Vakilov <dino_crizis@yahoo.com>
2024-09-11 19:04:42 +0200

    fixade stylning på ExamplePrompts o InputPrompts

--- src/components/inputFormPrompts/ExamplePrompts.jsx
+++ src/components/inputFormPrompts/ExamplePrompts.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from './ExamplePrompts.module.css';
 
 const ExamplePrompts = ({ prompts }) => {
   return (
-    <div>
-      <h3>Example Prompts</h3>
-      <ul>
+    <div className={styles.container}>
+      <h3 className={styles.h3}>Example Prompts</h3>
+      <ul className={styles.ul}>
         {prompts.map((prompt, index) => (
-          <li key={index}>{prompt}</li>
+          <li key={index} className={styles.li}>{prompt}</li>
         ))}
       </ul>
     </div>
--- src/components/inputFormPrompts/InputForm.jsx
+++ src/components/inputFormPrompts/InputForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './InputForm.module.css';
+import styles from './InputForm.module.css'; // Importera CSS-modulen som ett objekt
 
 const InputForm = ({ label, placeholder, onSubmit }) => {
     const [inputValue, setInputValue] = useState('');
@@ -13,17 +13,17 @@ const InputForm = ({ label, placeholder, onSubmit }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="input-form">
-            <label htmlFor="prompt-input" className="input-form-label">{label}</label>
+        <form onSubmit={handleSubmit} className={styles['input-form']}>
+            <label htmlFor="prompt-input" className={styles['input-form-label']}>{label}</label>
             <input
                 type="text"
                 id="prompt-input"
                 placeholder={placeholder}
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
-                className="input-form-input"
+                className={styles['input-form-input']}
             />
-            <button type="submit" className="input-form-button">
+            <button type="submit" className={styles['input-form-button']}>
                 Submit
             </button>
         </form>
