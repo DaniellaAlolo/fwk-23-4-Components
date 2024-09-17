082db248412e18a569f1d5e20a27c8ef87dd7981
sarah <sarah.emmoth@proton.me>
2024-09-17 14:26:51 +0200

    skapat Taskview som visar to-do board

---
+++ src/pages/to-do/TaskView.jsx
@@ -0,0 +1,14 @@
+import { Sidebar } from "../../components/Sidebar";
+import { TodoBoard } from "../../components/to-do";
+import styles from './TaskView.module.css'
+
+const TaskView = () => {
+    return ( 
+        <div className={styles.taskView}>
+            <Sidebar />
+            <TodoBoard />
+        </div>
+     );
+}
+ 
+export default TaskView;
