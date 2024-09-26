d809f649036f6d288db1813f2de363195a68cb0d
Sarah <sarah.emmoth@gmail.com>
2024-09-26 17:32:39 +0200

    created global css file with color palette. Doesnt work...edited styling on sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,33 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faSignInAlt, faTasks } from '@fortawesome/free-solid-svg-icons';  
-import styles from './Sidebar.module.css';  
-import React from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignInAlt, faTasks } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Sidebar.module.css";
+import React from "react";
+import { MdLogin, MdOutlineHome, MdChecklist } from "react-icons/md";
 const Sidebar = () => {
-  return (
-    <div className={styles.sidebar}>
-      <ul className={styles.sidebarList}>
-        <li className={styles.sidebarItem}>
-          <a href="#Home">
-            <FontAwesomeIcon icon={faHome} className={styles.icon} />
-            Home
-          </a>
-        </li>
-        <li className={styles.sidebarItem}>
-          <a href="#Login">
-            <FontAwesomeIcon icon={faSignInAlt} className={styles.icon} />
-            Login
-          </a>
-        </li>
-        <li className={styles.sidebarItem}>
-          <a href="#todo">
-            <FontAwesomeIcon icon={faTasks} className={styles.icon} />
-            To-do
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className={styles.sidebar}>
+            <ul className={styles.sidebarList}>
+                <li className={styles.sidebarItem}>
+                    <a href="/">
+                        <MdOutlineHome />
+                        Home
+                    </a>
+                </li>
+                <li className={styles.sidebarItem}>
+                    <a href="#Login">
+                        <MdLogin />
+                        Login
+                    </a>
+                </li>
+                <li className={styles.sidebarItem}>
+                    <a href="/dashboard">
+                        <MdChecklist />
+                        Tasks
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Sidebar;
