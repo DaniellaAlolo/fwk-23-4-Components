892fb40f0b47e00b991ea4585cbeb1326a2b3935
Sarah <sarah.emmoth@gmail.com>
2024-10-02 11:53:40 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { Chat } from "./components/chat";
 import { Footer } from "./components/footer";
-import { Header } from "./components/Header";
+import { Header } from "./components/header";
 import { InputForm } from "./components/inputFormPrompts";
 import { Message } from "./components/message";
 import { Modal } from "./components/taskModal";
-import { Navbar } from "./components/Navbar/";
-import { RegisterForm } from "./components/RegisterForm";
+import { Navbar } from "./components/navbar";
+import { RegisterForm } from "./components/registerForm";
 import { LoginForm } from "./components/login";
-import { Sidebar } from "./components/Sidebar";
+import { Sidebar } from "./components/sidebar";
 import { TodoBoard } from "./components/to-do";
 
 export {
