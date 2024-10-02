deec12157aa54b093fc9e9a8fd121b6f2f078e48
AriamGoitom <117870540+AriamGoitom@users.noreply.github.com>
2024-10-03 01:25:57 +0200

    changed background-color, add border-radius, changed padding, margin and margin-right

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,3 +14,29 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+/* // Navbar.jsx
+import styles from './Navbar.module.css'; 
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const Navbar = ({ onNavigate }) => {
+    return (
+        <nav className={styles.navbar}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <button onClick={() => onNavigate('home')}>Home</button>
+            </li>
+            <li className={styles.navItem}>
+              <button onClick={() => onNavigate('login')}>Login</button>
+            </li>
+            <li className={styles.navItem}>
+              <button onClick={() => onNavigate('todo')}>To-do</button>
+            </li>
+          </ul>
+        </nav>
+    );
+};
+
+export default Navbar; */
+
