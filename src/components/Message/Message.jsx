04e3aede615b5d1a5b6b1e20ff2f252a0c7ac5a9
sarah <sarah.emmoth@proton.me>
2024-10-03 10:09:33 +0200

    laggt till avatarcomponent for tydlighet i message

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,5 +1,6 @@
 import AiAtom from "./AiAtom";
 import styles from "./Message.module.css";
+import AvatarAtom from "../Header/AvatarAtom"
 import React from "react";
 
 const Message = ({ messages }) => {
@@ -8,7 +9,10 @@ const Message = ({ messages }) => {
             {messages.map((msg, index) => (
                 <div key={index} className={styles.messageContainer}>
                     {msg.type === 'user' ? (
-                        <p className={styles.userMessage}>{msg.text}</p>
+                        <>
+                            <AvatarAtom />
+                            <p className={styles.userMessage}>{msg.text}</p>
+                        </>
                     ) : (
                         <>
                             <AiAtom />
