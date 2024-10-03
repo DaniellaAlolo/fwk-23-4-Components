0226aae3f5f04637c715a6553756f5e110feb050
AriamGoitom <117870540+AriamGoitom@users.noreply.github.com>
2024-10-03 10:48:07 +0200

    fixed navigation in Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,38 +5,12 @@ const Navbar = () => {
     return (
         <nav className={styles.navbar}>
           <ul className={styles.navList}>
-          <li className={styles.navItem}><a href="#Home">Home</a></li>
-        <li className={styles.navItem}><a href="#Login">Login</a></li>
-        <li className={styles.navItem}><a href="#todo">To-do</a></li>
+          <li className={styles.navItem}><a href="/">Home</a></li>
+        <li className={styles.navItem}><a href="/login">Login</a></li>
+        <li className={styles.navItem}><a href="/dashboard">To-do</a></li>
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
-
-/* // Navbar.jsx
-import styles from './Navbar.module.css'; 
-import React from 'react';
-import LoginForm from './LoginForm';
-
-const Navbar = ({ onNavigate }) => {
-    return (
-        <nav className={styles.navbar}>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>
-              <button onClick={() => onNavigate('home')}>Home</button>
-            </li>
-            <li className={styles.navItem}>
-              <button onClick={() => onNavigate('login')}>Login</button>
-            </li>
-            <li className={styles.navItem}>
-              <button onClick={() => onNavigate('todo')}>To-do</button>
-            </li>
-          </ul>
-        </nav>
-    );
-};
-
-export default Navbar; */
-
