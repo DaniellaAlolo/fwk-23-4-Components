ea2e7123173a5c0f0fa222813a0d0f0ed05bb407
Daniella Alolo <daniella.alolo@hotmail.com>
2024-11-05 11:07:49 +0100

    Fixed error in AccountDeletion

--- src/components/DataDeletion/AccountDeletion.jsx
+++ src/components/DataDeletion/AccountDeletion.jsx
@@ -1,15 +1,22 @@
-import styles from "AccountDeletion.module.css";
-import { Btn } from "../Btn";
+import styles from "./AccountDeletion.module.css";
+import Btn from "../Btn/Btn.jsx";
 import { MdOutlineDelete } from "react-icons/md";
 
 const AccountDeletion = () => {
-    return (
-        <div className={styles.accSettingsWrapper}>
-            <h3>Delete your account</h3>
-            <p>Delete all your account and all associated information with your account.</p>
-            <Btn text={"Delete Account"} backgroundColor="red" icon={<MdOutlineDelete />} />
-        </div>
-    );
-}
+  return (
+    <div className={styles.accSettingsWrapper}>
+      <h3>Delete your account</h3>
+      <p>
+        Delete all your account and all associated information with your
+        account.
+      </p>
+      <Btn
+        text={"Delete Account"}
+        backgroundColor="red"
+        icon={<MdOutlineDelete />}
+      />
+    </div>
+  );
+};
 
 export default AccountDeletion;
