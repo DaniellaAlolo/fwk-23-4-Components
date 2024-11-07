da460b38dd2440a0f6660cf41f2d2d6bce0dcbad
sarah <sarah.emmoth@proton.me>
2024-11-07 14:17:24 +0100

    uppdaterat site key

--- src/components/Captcha/Captcha.jsx
+++ src/components/Captcha/Captcha.jsx
@@ -5,7 +5,7 @@ const Captcha = ({ onCaptchaInputChange }) => {
     if (window.grecaptcha) {
       window.grecaptcha.enterprise.ready(() => {
         window.grecaptcha.enterprise.render("recaptcha-container", {
-          sitekey: "6LectHcqAAAAAObYvabpFgmiNjfkuHTqEcI8Vjme", 
+          sitekey: "6Ld793cqAAAAAKK85uBqw2cNDFNns9zTT2Va0Z4m", 
           callback: (token) => onCaptchaInputChange(token), 
         });
       });
